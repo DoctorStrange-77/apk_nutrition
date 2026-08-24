0577a9c5303f225778f56372adce21d24b644dc7
DoctorStrange-77 <salvatore.carotenuto77@gmail.com>
2026-08-24 17:22:36 +0200

    ui: load calendar styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 import './styles-v07.css';
+import './styles-v07-calendar.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
